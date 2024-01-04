c5d79aafbfef6a99fd2eeeb1bf6d5088c0c7b47d
Onur Bostan <onurbosstan@gmail.com>
2024-01-04 21:58:12 +0300

    password reset page completed

--- BossiaNews/views/ForgotPassword.swift
+++ BossiaNews/views/ForgotPassword.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class ForgotPassword: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    let viewModel = NewsRepo()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func resetButton(_ sender: Any) {
+        if let email = self.emailText.text {
+            self.viewModel.forgotPassword(email: email) { error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: "You entered the wrong e-mail address")
+                } else {
+                    self.makeAlert(titleInput: "Success!", messageInput: "A link has been sent to your e-mail address to reset your password.")
+                }
+            }
+        }
     }
-    */
-
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
 }
