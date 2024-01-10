b44d57da8e04abf155210ddc6a942a3c6657bbff
Onur Bostan <onurbosstan@gmail.com>
2024-01-10 17:03:34 +0300

    when the logged-in user closes the application and opens it again, the process of being sent directly to the page without the need for login is completed

--- BossiaNews/views/LogIn.swift
+++ BossiaNews/views/LogIn.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LogIn: UIViewController {
-
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    let viewModel = LogInViewModel()
+    var iconClick = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        let currentUser = Auth.auth().currentUser
+        if currentUser != nil {
+            self.performSegue(withIdentifier: "toHomeVC", sender: nil)
+        }
     }
-
-
+    
+    @objc func closeKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func logInButton(_ sender: Any) {
+        if let email = emailText.text, let password = passwordText.text {
+            viewModel.signIn(email: email, password: password) { error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: "Username or password is wrong. Please try again, if you do not have an account, please register.")
+                } else {
+                    self.performSegue(withIdentifier: "toHomeVC", sender: nil)
+                }
+            }
+        }
+    }
+    @IBAction func hidePasswordButton(_ sender: Any) {
+        if iconClick {
+            passwordText.isSecureTextEntry = false
+        } else {
+            passwordText.isSecureTextEntry = true
+        }
+        iconClick = !iconClick
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
 }
 
