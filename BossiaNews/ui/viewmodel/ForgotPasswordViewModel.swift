020ccc8d41376d7d06ff36601836dd1d1c4b0963
Onur Bostan <onurbosstan@gmail.com>
2024-01-14 00:44:59 +0300

    function fixed

--- BossiaNews/ui/viewmodel/ForgotPasswordViewModel.swift
+++ BossiaNews/ui/viewmodel/ForgotPasswordViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class ForgotPasswordViewModel {
+    var nrepo = NewsRepo()
+    func forgotPassword(email:String, completion: @escaping (Error?) -> Void) {
+        nrepo.forgotPassword(email: email) { error in
+            if let error = error {
+                completion(error)
+            } else {
+                completion(nil)
+            }
+        }
+    }
+}
